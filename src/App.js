020b8fdb7ba3a32bf71757ade1f5b3a15727368f
kdilot <kdilot@gmail.com>
2019-06-04 13:39:32 -0700

    index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { PosContainer } from 'redux/container';
+import { ScoreBoardContainer } from 'redux02/container';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-     <Route exact path='/' component={PosContainer} />
-     <Route path='/pos' component={PosContainer} />
+      <Route exact path='/' component={PosContainer} />
+      <Route path='/pos' component={PosContainer} />
+      <Route path='/score' component={ScoreBoardContainer} />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
-import store from 'redux/store';
+import store from 'store';
 
 ReactDOM.render(
   <Provider store={store}>
