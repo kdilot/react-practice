d1ada48b12d1b8beec5d2c81388a33345106f789
kdilot <kdilot@gmail.com>
2019-08-07 14:20:53 -0700

    widget

--- src/use/widgets/index.js
+++ src/use/widgets/index.js
@@ -2,4 +2,5 @@ export { default as Button } from './button/Button';
 export { default as ButtonExample } from './button/ButtonExample';
 export { default as GridForm } from './grid/GridForm';
 export { default as List } from './list/List';
+export { default as Popup } from './popup/Popup';
 export { default as Tab } from './tab/Tab';
