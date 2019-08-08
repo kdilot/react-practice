8ba3eef4937a68a71d5f1d4d572535a65dba7c2b
kdilot <kdilot@gmail.com>
2019-08-08 13:39:59 -0700

    widget

--- src/use/widgets/popup/Popup.js
+++ src/use/widgets/popup/Popup.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Popup.module.scss';
 
 const Popup = ({ children, effects }) => {
+  const [test, setTest] = useState(true)
   return (
     <>
-      <div className={`${styles.wrapper} ${styles[effects]}`}>
-        <div className={`${styles.popup} ${styles[effects]}`}>
-          {children}
+      {test &&
+        <div className={`${styles.wrapper} ${styles[effects]}`}>
+          <div className={`${styles.popup} ${styles[effects]}`}>
+            <button onClick={() => { setTest(!test) }}>asdfasd</button>
+            {children}
+            {test ? 1 : 0}
+          </div>
         </div>
-      </div>
+      }
     </>
   );
 };
