a0f7c39cc4ee8684dc7b61bf2f2127c7ec232d3e
kdilot <kdilot@gmail.com>
2019-08-10 16:50:23 -0700

    widget

--- src/use/widgets/popup/Popup.js
+++ src/use/widgets/popup/Popup.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Popup.module.scss';
 
-const Popup = ({ children, effects, button = 'Button' }) => {
+const Popup = ({ children, effects = 'left', button = 'Button' }) => {
   const [display, setDisplay] = useState(null)
+  const keyESC = event => {
+    if (event.key === 'Escape') setDisplay('off')
+  }
+  useEffect(() => {
+    window.addEventListener('keydown', keyESC)
+  }, [])
   return (
     <>
-      <div className={styles.popupButton} onClick={() => { setDisplay('on') }}>{button}</div>
+      <div onClick={() => { setDisplay('on') }}>{button}</div>
       <div className={`${styles.wrapper} ${styles[effects]} ${display === 'on' && styles.on} ${display === 'off' && styles.off}`}>
-        <div className={styles.popupButton} onClick={() => { setDisplay('off') }}>{button}</div>
+        <div className={styles.popupButton} onClick={() => { setDisplay('off') }}>Press the ESC or click to close</div>
         <div className={`${styles.popup} ${styles[effects]} ${display === 'on' && styles.on} ${display === 'off' && styles.off}`}>
           {children}
         </div>
